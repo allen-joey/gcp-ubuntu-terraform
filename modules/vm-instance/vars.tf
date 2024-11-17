4a2670bbae909172b7d9a7d92eb9fe1efb3ad82a
Joey Allen <joey.allen@gmx.com>
2024-11-17 19:41:25 +0000

    updated vars.tf

--- modules/vm-instance/vars.tf
+++ modules/vm-instance/vars.tf
@@ -10,13 +10,13 @@ variable "instance-size" {
 
 variable "id" {
   description = "ID of a Google Cloud Project. Can be omitted and will be generated automatically"
-  default     = "gcp-001-440816"
+  default     = ""
 }
 
 variable "project_id" {
   type        = string
   description = "project id"
-  default     = "gcp-001-440816"
+  default     = ""
 }
 
 variable "region" {
